7072244ae213f2ccbce501eb2cda19196c5cabbc
FelipePinheiro <fsp12371@git.com>
2024-08-16 16:56:19 -0300

    Banco de Dados parcialmente corrigido

--- src/main/resources/db/migration/V1__create_table_users.sql
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
     id bigint not null,
     name varchar(255),
-    registrationDate Date,
-    isSatisfied boolean,
+    registration_date Date,
+    is_satisfied boolean,
     gender varchar(10),
     aged boolean,
-    timeOfService bigint,
-    exitForecast Date,
+    time_of_service bigint,
+    exit_forecast Date,
     primary key(id)
 );
