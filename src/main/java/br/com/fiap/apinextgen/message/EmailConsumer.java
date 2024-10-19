57d1d0457345a3ba85a99c6f9c05eb0ad8cc938a
FelipePinheiro <fsp12371@git.com>
2024-10-19 11:51:11 -0300

    SMTP funcionando

--- src/main/java/br/com/fiap/apinextgen/message/EmailConsumer.java
+++ src/main/java/br/com/fiap/apinextgen/message/EmailConsumer.java
@@ -1,15 +1,23 @@
 package br.com.fiap.apinextgen.message;
 
 import br.com.fiap.apinextgen.ApiNextgenApplication;
+import jakarta.mail.MessagingException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    private final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = ApiNextgenApplication.FEEDBACK_QUEUE)
-    public void receiveMessage(String message) {
+    public void receiveMessage(String message) throws MessagingException {
         System.out.println("Received <" + message + ">");
+        emailService.sendEmail(message);
     }
 
 }
--- src/main/java/br/com/fiap/apinextgen/message/EmailService.java
+++ src/main/java/br/com/fiap/apinextgen/message/EmailService.java
@@ -1,12 +1,29 @@
 package br.com.fiap.apinextgen.message;
 
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    public void sendEmail(String message) {
+    @Autowired
+    JavaMailSender mailSender;
 
+    public void sendEmail(String message) throws MessagingException {
+        var email = mailSender.createMimeMessage();
+        var config = new MimeMessageHelper(email);
+
+        config.setTo("fsp12371@gmail.com");
+        config.setSubject("Feedback realizado com sucesso!");
+        config.setText("""
+                    <h1>Novo Feedback</h1>
+                    <p>%s</p>                
+                """.formatted(message), true);
+
+        mailSender.send(email);
     }
 
 }
